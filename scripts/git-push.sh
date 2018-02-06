c94e7761dbad98cc3d1952516329d8c52e5ea660
agusgs <garciasmithagustin@gmail.com>
2018-02-06 12:23:35 -0300

    manejo de error en script de push

--- scripts/git-push.sh
+++ scripts/git-push.sh
@@ -3,6 +3,18 @@
 echo Subiendo a git los cambios de version
 
 git push
+RET=$?
+if [ 0 != $RET ]; then
+    echo ERROR: No se pudo puehear. Error code: $RET
+    return 1
+fi
+
 git push --tags
+RET=$?
+if [ 0 != $RET ]; then
+    echo ERROR: No se pudo puehear. Error code: $RET
+    return 1
+fi
 
 echo Versiones actualizadas en repo remoto
+
