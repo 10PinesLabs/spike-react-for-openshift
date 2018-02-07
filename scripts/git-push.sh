ae61a49b2834c2047b3b292924628168d1632e08
agusgs <garciasmithagustin@gmail.com>
2018-02-07 14:06:15 -0300

    agrego url con ssh de git para pushear desde jenkins

--- scripts/git-push.sh
+++ scripts/git-push.sh
@@ -2,14 +2,17 @@
 
 echo Subiendo a git los cambios de version
 
-git push
+GIT_URL=$(npm run git-connection --silent)
+echo PUSH: $GIT_URL
+
+git push $GIT_URL
 RET=$?
 if [ 0 != $RET ]; then
     echo ERROR: No se pudo puehear. Error code: $RET
     exit 1
 fi
 
-git push --tags
+git push --tags $GIT_URL
 RET=$?
 if [ 0 != $RET ]; then
     echo ERROR: No se pudo puehear. Error code: $RET
