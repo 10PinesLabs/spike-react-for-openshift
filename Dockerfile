e0fae205352ad3b4c9b3157a15b9a01890d2ceb0
agusgs <garciasmithagustin@gmail.com>
2018-02-08 13:54:21 -0300

    configuracion de nginx para levantarlo con docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM nginx
 
 COPY start.sh /tmp/
-
+COPY build /data/www
 ENV URL_CONFIGURACION http://127.0.0.1:8081/spikereact/development
 ENV URL_BACKEND http://127.0.0.1:8080
 
--- start.sh
+++ start.sh
@@ -3,26 +3,32 @@
 echo "Armando el archivo de configuración de ngnix"
 
 echo "
-server {
-	listen 80; 
-	server_name _; 
-	root /home/gi/dev/git-claro/spike-react-for-openshift/build; 
-	index index.html; 
-	location / { 
-		try_files $uri /index.html; 
-    }
-    location /config {
-        proxy_pass ${URL_CONFIGURACION};
-	proxy_set_header Host $host;
-	proxy_set_header X-Real-IP $remote_addr;
-    	proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-	proxy_set_header content-type "application/json";
-    }
-    location /back {
-	rewrite ^/back(.*) /$1 break;
-        proxy_pass ${URL_BACKEND};
+http {
+    server {
+        listen 80;
+        server_name _;
+        index index.html;
+        root /data/www;
+        location / {
+	    	try_files \$uri /index.html;
+        }
+        location /config {
+            proxy_pass ${URL_CONFIGURACION};
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header content-type "application/json";
+        }
+        location /back {
+        rewrite ^/back(.*) /\$1 break;
+            proxy_pass ${URL_BACKEND};
+        }
     }
 }
+events {
+    worker_connections  1024;
+}
+
 #events {
 #    worker_connections  1024;
 #}
@@ -39,5 +45,7 @@ server {
 #	include /etc/nginx/conf.d/*.conf;
 #}
 " > /etc/nginx/nginx.conf
+
+cat /etc/nginx/nginx.conf
 
 echo "CONFIG generada"
